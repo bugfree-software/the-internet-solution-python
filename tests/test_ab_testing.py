d142bed6916d8b34509c12623b4802eca9206695
Tytus Dobrzynski <tytus.dobrzynski@gmail.com>
2014-07-29 18:59:18 +0200

    Make the AB test case more stable.

--- tests/test_ab_testing.py
+++ tests/test_ab_testing.py
@@ -5,16 +5,18 @@ class AbTestingTest(TheInternetTestCase):
 	def get_page(self):
 		return "http://the-internet.herokuapp.com/abtest"
 	def test_ab_variates(self):
-		variation = S("h3")
-		first_variation = variation.web_element.text
+		header = S("h3")
+		first_variation = header.web_element.text
 		self.assertIn(
 			first_variation, [u"A/B Test Variation 1", u"A/B Test Control"]
 		)
-		get_driver().delete_all_cookies()
-		go_to("http://the-internet.herokuapp.com/abtest")
-		variation = S("h3")
-		second_variation = variation.web_element.text
-		self.assertIn(
-			second_variation, [u"A/B Test Variation 1", u"A/B Test Control"]
-		)
+		second_variation = first_variation
+		while second_variation == first_variation:
+			get_driver().delete_all_cookies()
+			go_to("http://the-internet.herokuapp.com/abtest")
+			header = S("h3")
+			second_variation = header.web_element.text
+			self.assertIn(
+				second_variation, [u"A/B Test Variation 1", u"A/B Test Control"]
+			)
 		self.assertNotEqual(first_variation, second_variation)
