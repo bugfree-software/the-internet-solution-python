562b56d67d7d292d7c63ec8c3f453bae92a3b073
Tytus Dobrzynski <tytus.dobrzynski@gmail.com>
2014-07-30 12:35:16 +0200

    Simplify the WYSIWYG editor test case.

--- tests/test_wysiwyg_editor.py
+++ tests/test_wysiwyg_editor.py
@@ -1,16 +1,12 @@
 from . import TheInternetTestCase
-from helium.api import click, Text, press, CONTROL, COMMAND, write
-from sys import platform
+from helium.api import click, Text, write
 
 class WYSIWYGEditorTest(TheInternetTestCase):
 	def get_page(self):
 		return "http://the-internet.herokuapp.com/tinymce"
 	def test_use_wysiwyg_editor(self):
 		self.assertTrue(Text("Your content goes here.").exists())
-		click("Your content goes here.")
-		if platform == 'darwin':
-			press(COMMAND + 'a')
-		else:
-			press(CONTROL + 'a')
+		click("File")
+		click("New document")
 		write("Hello Helium!")
 		self.assertTrue(Text("Hello Helium!").exists())
