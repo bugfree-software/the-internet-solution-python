6fa314f29d1036a89e43e8a1c7152911f0ffe430
Tytus Dobrzynski <tytus.dobrzynski@gmail.com>
2014-07-30 09:59:41 +0200

    Add test case for geolocation.

---
+++ tests/test_geolocation.py
@@ -0,0 +1,22 @@
+from . import TheInternetTestCase
+from helium.api import click, get_driver, S
+
+class AbTestingTest(TheInternetTestCase):
+	def get_page(self):
+		return "http://the-internet.herokuapp.com/geolocation"
+	def test_fake_geolocation(self):
+		get_driver().execute_script(
+			'window.navigator.geolocation.getCurrentPosition = '
+				'function(success){ '
+					'var position = {"coords" : { '
+						'"latitude": "1", '
+						'"longitude": "2"'
+						'}'
+				'}; '
+			'success(position);}'
+		)
+		click('Where am I?')
+		latitude = S("#lat-value").web_element.text
+		self.assertEqual(latitude, u"1")
+		longitude = S("#long-value").web_element.text
+		self.assertEqual(longitude, u"2")
